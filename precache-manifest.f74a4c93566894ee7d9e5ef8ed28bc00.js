0c0d09f9a43773da7448bc722d81926b47e534c6
ruby840124 <ruby840124@gmail.com>
2019-10-22 10:50:01 +0800

    Updates

---
+++ precache-manifest.f74a4c93566894ee7d9e5ef8ed28bc00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb19f111731e735ce17847d5b2a947e2",
+    "url": "/TWHeadlines/index.html"
+  },
+  {
+    "revision": "d9baf9b41379c01845e6",
+    "url": "/TWHeadlines/static/css/main.f82b9b22.chunk.css"
+  },
+  {
+    "revision": "2a6e115bcb62ee8ac053",
+    "url": "/TWHeadlines/static/js/2.7ca2f5a8.chunk.js"
+  },
+  {
+    "revision": "d9baf9b41379c01845e6",
+    "url": "/TWHeadlines/static/js/main.1380c742.chunk.js"
+  },
+  {
+    "revision": "56198b586ced56f0308c",
+    "url": "/TWHeadlines/static/js/runtime-main.2a790979.js"
+  }
+]);
