94aeeb331617296bf6265f6c8f63198ea1b3ef0c
ruby840124 <ruby840124@gmail.com>
2019-10-29 10:41:56 +0800

    Updates

---
+++ precache-manifest.dcd1c0ad626135ab2803498753bbf583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fcada7cf279b08b518be03533467917",
+    "url": "/TWHeadlines/index.html"
+  },
+  {
+    "revision": "53b77ec808e551b3442c",
+    "url": "/TWHeadlines/static/css/main.f82b9b22.chunk.css"
+  },
+  {
+    "revision": "281bcbd0b9af19d68639",
+    "url": "/TWHeadlines/static/js/2.2b3d5cf3.chunk.js"
+  },
+  {
+    "revision": "53b77ec808e551b3442c",
+    "url": "/TWHeadlines/static/js/main.f4420b08.chunk.js"
+  },
+  {
+    "revision": "56198b586ced56f0308c",
+    "url": "/TWHeadlines/static/js/runtime-main.2a790979.js"
+  },
+  {
+    "revision": "79164d53a0082833175e7877fb2d5c9b",
+    "url": "/TWHeadlines/static/media/cat.79164d53.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TWHeadlines/precache-manifest.eac648fae353e122feb3bf3822249593.js"
+  "/TWHeadlines/precache-manifest.dcd1c0ad626135ab2803498753bbf583.js"
 );
 
 self.addEventListener('message', (event) => {
